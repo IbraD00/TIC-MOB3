a3f473957863ffca323d03d31a8addd5fb4337f7
Ibra <ibrahima.dansokho@outlook.com>
2017-12-20 06:41:19 +0100

    Parse ok

--- AppWeather/components/UploadView.js
+++ AppWeather/components/UploadView.js
@@ -14,112 +14,131 @@ import Content from '../Layout/Content';
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 class UploadView extends Component {
-    constructor() {
-      super();
-      this.state = {
-        loading: false,
-        data: "",
-        title: 'ibra',
-        files: [{
-          name: 'text.txt',
-          type: 'text'
-        }]
-      };
-      this.browse.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      content: [],
+      data: "",
+      title: 'ibra',
+      files: [{
+        name: 'text.txt',
+        type: 'text'
+      }]
+    };
+    this.browse.bind(this);
+  }
 
-    browse() {
-      let self = this;
-      DocumentPicker.show({
-        filetype: [DocumentPickerUtil.allFiles(), 'public.content', 'public.composite-content'],
-      },(error,res) => {
-        let index = _.findIndex(this.state.files, (o) => { return o.name === res.fileName });
-        if (index === -1) {
-          this.setState({
-            loading: true,
-          }, () => {
-            fetch(res.uri)
-            .then(function(response) {
-              if (response.status === 200) {
-                console.log(response);
-                self.setState({
-                  title: response._bodyText[3],
-                  data: response._bodyText,
-                  files: _.concat(self.state.files, {
-                    name: res.fileName,
-                    type: res.type
-                  })
-                }, () => {
-                  self.setState({
-                    loading: false,
-                  })
-                });
-              } else {
+  browse() {
+    let self = this;
+    DocumentPicker.show({
+      filetype: [
+        DocumentPickerUtil.allFiles(),
+        'public.content',
+        'public.composite-content'
+      ],
+    }, (error, res) => {
+      let index = _.findIndex(this.state.files, (o) => { return o.name === res.fileName });
+      if (index === -1) {
+        this.setState({
+          loading: true,
+        }, () => {
+          fetch(res.uri)
+          .then((response) => {
+            if (response.status === 200) {
+              console.log(response);
+              self.setState({
+                data: response._bodyText,
+                values: [],
+                files: _.concat(self.state.files, {
+                  name: res.fileName,
+                  type: res.type
+                })
+              }, () => {
                 self.setState({
                   loading: false,
-                })
-              }
-            });
-          });
-          // console.log(
-          //   res.uri,
-          //   res.type, // mime type
-          //   res.fileName,
-          //   res.fileSize
-          // );
-        }
-      });
-    }
+                }, () => {
+                  let value = this.state.data;
+                  let content = [];
 
-    removeFile(index) {
-      this.setState({
-        loading: true,
-        files: update(this.state.files, { $splice: [[index, 1]] })
-      }, () => {
-        this.setState({
-          loading: false
-        });
-      });
-    }
-
-    render() {
-      const { title, files, loading } = this.state;
-      return (
-        <Body title="Bienvenue dans AppWeather">
-          <If condition={loading}>
-            <Then>
-              <ActivityIndicator size="large" color="#000" />
-            </Then>
-          </If>
-          <Button
-            raised
-            onPress={this.browse.bind(this)}
-            icon={{name: 'cloud', size: 16}}
-            buttonStyle={styleBtn.btnPrimary}
-            textStyle={styleBtn.btnPrimaryText}
-            title={`Upload`}
-          />
-          <Content>
-            <Text h4>Historique {title}</Text>
-            <ScrollView>
-                <List containerStyle={{marginBottom: 20}}>
-                  {
-                    files.map((item, index) => (
-                      <ListItem
-                        key={index}
-                        title={item.name}
-                        subtitle={item.type}
-                        onPressRightIcon={() => this.removeFile(index)}
-                        rightIcon={{name: 'close'}}
-                      />
-                    ))
+                  for (let i = 1; i < 2; i++) {
+                    data = value.split('\n')[i].split('\t');
+                    for (let y = 0; y < data.length; y++) {
+                      content[data[y]] = [];
+                      for (let x = 2; x < data.length; x++) {
+                        datax = value.split('\n')[x].split('\t');
+                        content[data[y]].push(datax[y]);
+                      }
+                    }
                   }
-                </List>
-            </ScrollView>
-          </Content>
-        </Body>
-      );
-    }
+
+                  console.log(content);
+
+                  this.setState({
+                    content
+                  }, () => { });
+                });
+              });
+            } else {
+              self.setState({
+                loading: false,
+              });
+            }
+          });
+        });
+      }
+    });
+  }
+
+  removeFile(index) {
+    this.setState({
+      loading: true,
+      files: update(this.state.files, { $splice: [[index, 1]] })
+    }, () => {
+      this.setState({
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    const { title, files, loading } = this.state;
+    return (
+      <Body title="Bienvenue dans AppWeather">
+        <If condition={loading}>
+          <Then>
+            <ActivityIndicator size="large" color="#000" />
+          </Then>
+        </If>
+        <Button
+          raised
+          onPress={this.browse.bind(this)}
+          icon={{ name: 'cloud', size: 16 }}
+          buttonStyle={styleBtn.btnPrimary}
+          textStyle={styleBtn.btnPrimaryText}
+          title={`Upload`}
+        />
+        <Content>
+          <Text h4>Historique {title}</Text>
+          <ScrollView>
+            <List containerStyle={{ marginBottom: 20 }}>
+              {
+                files.map((item, index) => (
+                  <ListItem
+                    key={index}
+                    title={item.name}
+                    subtitle={item.type}
+                    onPressRightIcon={() => this.removeFile(index)}
+                    rightIcon={{ name: 'close' }}
+                  />
+                ))
+              }
+            </List>
+          </ScrollView>
+        </Content>
+      </Body>
+    );
+  }
 }
 
 export default UploadView;
